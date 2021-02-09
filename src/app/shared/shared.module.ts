1043af2a4adc06274231a7253847b5eb81b1400f
José Carlos Palma <palmahn@gmail.com>
2021-02-08 20:55:25 -0600

    Creación del componente sidenav en el módulo shared.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SidenavComponent } from './sidenav/sidenav.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [SidenavComponent],
   imports: [
     CommonModule
   ]
