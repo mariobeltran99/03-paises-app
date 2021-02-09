49392655f05369039093bf97ec3921d6224c0ca2
José Carlos Palma <palmahn@gmail.com>
2021-02-08 21:09:58 -0600

    Corrección de la página buscar por nombre de país al módulo country.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ByCountryComponent } from './pais/pages/by-country/by-country.component';
-import { ByRegionComponent } from './pais/pages/by-region/by-region.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ByCountryComponent,
-    ByRegionComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/country/country.module.ts
+++ src/app/country/country.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+
 import { ByCapitalComponent } from './pages/by-capital/by-capital.component';
+import { ByCountryComponent } from './pages/by-country/by-country.component';
+import { ByRegionComponent } from './pages/by-region/by-region.component';
 
 
 
 @NgModule({
-    declarations: [ByCapitalComponent],
+    declarations: [
+        ByCapitalComponent,
+        ByCountryComponent,
+        ByRegionComponent
+    ],
     imports: [
         CommonModule
     ]
