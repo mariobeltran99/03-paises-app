d24f73763d04626f194353a59676ee273f474e2e
José Carlos Palma <palmahn@gmail.com>
2021-02-08 21:11:39 -0600

    Creación de la página para mostrar información del país.

--- src/app/country/country.module.ts
+++ src/app/country/country.module.ts
@@ -5,6 +5,7 @@ import { CommonModule } from '@angular/common';
 import { ByCapitalComponent } from './pages/by-capital/by-capital.component';
 import { ByCountryComponent } from './pages/by-country/by-country.component';
 import { ByRegionComponent } from './pages/by-region/by-region.component';
+import { CountryComponent } from './pages/country/country.component';
 
 
 
@@ -12,7 +13,8 @@ import { ByRegionComponent } from './pages/by-region/by-region.component';
     declarations: [
         ByCapitalComponent,
         ByCountryComponent,
-        ByRegionComponent
+        ByRegionComponent,
+        CountryComponent
     ],
     imports: [
         CommonModule
