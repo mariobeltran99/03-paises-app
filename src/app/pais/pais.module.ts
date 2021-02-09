57fa1a4f40b3b5d14dbdc5848fa77a0454e7c0df
José Carlos Palma <palmahn@gmail.com>
2021-02-08 20:58:53 -0600

    Creación de la página para buscar por capital en el módulo pais.

--- src/app/pais/pais.module.ts
+++ src/app/pais/pais.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ByCapitalComponent } from './pages/by-capital/by-capital.component';
 
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+    declarations: [ByCapitalComponent],
+    imports: [
+        CommonModule
+    ]
 })
 export class PaisModule { }
