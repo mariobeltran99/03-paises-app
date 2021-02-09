3518fa69f6adecc74d4f33bf0f030a84109c8cbd
José Carlos Palma <palmahn@gmail.com>
2021-02-08 21:50:09 -0600

    Configuración de rutas principales.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { CountryModule } from './country/country.module';
@@ -11,6 +12,7 @@ import { CountryModule } from './country/country.module';
     ],
     imports: [
         BrowserModule,
+        AppRoutingModule,
         SharedModule,
         CountryModule
     ],
