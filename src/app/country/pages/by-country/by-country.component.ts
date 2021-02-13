3ace17e0f3a9b4beda000a270b4e8e50740e03a3
José Carlos Palma <palmahn@gmail.com>
2021-02-12 22:09:47 -0600

    Implementación de sugerencia en búsqueda por país.

--- src/app/country/pages/by-country/by-country.component.ts
+++ src/app/country/pages/by-country/by-country.component.ts
@@ -7,20 +7,27 @@ import { CountryService } from '../../services/country.service';
 @Component({
     selector: 'app-by-country',
     templateUrl: './by-country.component.html',
-    styles: [
-    ]
+    styles: [`
+        ul li {
+            cursor: pointer;
+        }
+    `]
 })
 export class ByCountryComponent {
 
     public term = '';
     public notFound = false;
     public countries: Country[] = [];
+    public suggestions: Country[] = [];
+    public show = false;
 
     constructor(private countryService: CountryService) { }
 
     public find(term: string): void {
         this.term = term;
+        this.show = false;
         this.notFound = false;
+        this.suggestions = [];
         this.countryService.findCountry(term)
             .subscribe(countries => {
                 this.countries = countries;
@@ -31,10 +38,17 @@ export class ByCountryComponent {
 
     }
 
-    public suggestion(term: string): void {
+    public getSuggestions(term: string): void {
         this.notFound = false;
-        // TODO: Crear sugerencias.
-        console.log(term);
+        this.show = true;
+        this.term = term;
+        this.countryService
+            .findCountry(term)
+            .subscribe(
+                countries => this.suggestions = countries.splice(0, 5),
+                (err) => this.suggestions = []
+            );
+
     }
 
 }
